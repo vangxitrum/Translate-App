11253544de0e063cf99d4542b4875b4d276aa815
QuangTuan <tuantq666@gmail.com>
2021-05-25 16:12:40 +0700

    change searchbar

--- Dictionary_UI/app/src/main/java/com/example/dictionary_ui/MainActivity.java
+++ Dictionary_UI/app/src/main/java/com/example/dictionary_ui/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.dictionary_ui;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 
 import java.util.ArrayList;
@@ -24,26 +28,21 @@ public class MainActivity extends AppCompatActivity {
     CardView rememberGame;
     CardView cardGame;
     CardView history;
-
-
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.result_list);
-        List<String> myList = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,myList);
-        listView.setAdapter(arrayAdapter);
         setting = findViewById(R.id.settingBt);
         rememberGame = findViewById(R.id.rememberGameBt);
         cardGame = findViewById(R.id.cardGameBt);
         history = findViewById(R.id.historyBt);
-        Intent historyIt = new Intent(this,history.class);
-        Intent settingIt = new Intent(this,setting.class);
-        Intent rememberIt = new Intent(this,rememberGame.class);
-        Intent cardGameIt = new Intent(this,cardGame.class);
+        Intent historyIt = new Intent(this, history.class);
+        Intent settingIt = new Intent(this, setting.class);
+        Intent rememberIt = new Intent(this, rememberGame.class);
+        Intent cardGameIt = new Intent(this, cardGame.class);
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,28 +73,56 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.nav_menu,menu);
-        MenuItem menuItem = menu.findItem(R.id.nav_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView = findViewById(R.id.search_view_id);
+        ListView listView = findViewById(R.id.rs_list);
+        List<String> myList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,myList);
+        listView.setAdapter(arrayAdapter);
+        myList.add("test");
+        myList.add("yasuo");
+        myList.add("zed");
+        listView.setAdapter(arrayAdapter);
         searchView.setQueryHint("Search Here");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+        searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+            ConstraintLayout temp = findViewById(R.id.main_bg);
             @Override
             public boolean onQueryTextSubmit(String query) {
+                listView.setVisibility(View.GONE);
+
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                listView.setVisibility(View.VISIBLE);
                 arrayAdapter.getFilter().filter(newText);
                 return true;
             }
         });
 
-        return super.onCreateOptionsMenu(menu);
-    }}
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            ConstraintLayout temp = findViewById(R.id.main_bg);
+            @Override
+            public boolean onClose() {
+                listView.setVisibility(View.GONE);
+                return false;
+            }
+        });
+
+        /*searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!searchView.hasFocus()){
+                    searchView.setIconifiedByDefault(true);
+                    searchView.onActionViewExpanded();
+
+                }
+
+            }
+        });*/
+
+
+    }
+}
--- Dictionary_UI/app/src/main/java/com/example/dictionary_ui/rememberGame.java
+++ Dictionary_UI/app/src/main/java/com/example/dictionary_ui/rememberGame.java
@@ -3,12 +3,26 @@ package com.example.dictionary_ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class rememberGame extends AppCompatActivity {
 
     ImageButton backBt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
